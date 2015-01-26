549e47e36446fd04de6da1daf28c03f183e4e1cf
Simon <simon.j.goring@gmail.com>
2015-01-26 12:45:04 -0600

    Finished revisions and changed figure number ordering.
    Ignored the `cache` and `figure` folders, they'll get added in the `Makefile` process (also commited), no need to explicitly store them with git.

---
+++ R/paper/R/figures/fig10_transect_plots.R
@@ -0,0 +1,135 @@
+#  Transect axes:
+transects <- list(one = data.frame(x = c(-18093, 631094),
+                                   y = c(1470422, 668889)),
+                  two = data.frame(x = c(-48780, 675121),
+                                   y = c(832084, 1151701)))
+
+get_cells <- function(x){
+  cells <- extract(num.rast, 
+                   data.frame(x = seq(x$x[1], x$x[2], length.out = 500),
+                              y = seq(x$y[1], x$y[2], length.out = 500)))
+  
+  cells <- cells[!duplicated(cells)]
+  cells
+}
+
+trans.cells <- lapply(transects, get_cells)
+
+get_points <- function(x){
+  cells <- extract(num.rast, 
+                   data.frame(x = seq(x$x[1], x$x[2], length.out = 500),
+                              y = seq(x$y[1], x$y[2], length.out = 500)))
+  
+  cells <- cells[!duplicated(cells)]
+  
+  transect.plss <- composition.table[match(cells, composition.table$cell),3:ncol(composition.table)]
+  transect.fia <- agg.dens[match(cells, agg.dens$cell),]
+  
+  transect.plss$cell <- xyFromCell(num.rast, transect.plss$cell)[,1]
+  transect.fia$cell <- xyFromCell(num.rast, transect.fia$cell)[,1]
+
+  transect.plss$class <- 'PLSS'
+  transect.fia$class <- 'FIA'
+  
+  transect.fia[,2:(ncol(transect.fia)-1)] <- transect.fia[,2:(ncol(transect.fia)-1)] / rowSums(transect.fia[,2:(ncol(transect.fia)-1)], na.rm=TRUE)
+  
+  good.taxa <- c('cell', 'class', 'Tamarack', 'Pine', 'Birch', 'Elm', 
+                 'Maple', 'Oak', 'Poplar', 'Spruce', 'Hemlock')
+    
+  sample.out <- rbind(melt(transect.plss[,good.taxa],
+                           id.vars = c('cell', 'class')),
+                      melt(transect.fia[,good.taxa],
+                           id.vars = c('cell', 'class')))
+  
+  sample.out$value[is.na(sample.out$value)] <- 0
+  
+  sample.out$PFT <- biom.table$PFT[match(sample.out$variable, biom.table[,1])]
+  sample.out$PFT <- gsub(pattern = 'Temperate', replacement = '', x = sample.out$PFT)
+  sample.out$PFT <- gsub(pattern = 'Tree', replacement = '', x = sample.out$PFT)
+  
+  sample.out          
+}
+
+transect.taxa <- lapply(transects, get_points)
+transect.taxa[[1]]$transect <- 'One'
+transect.taxa[[2]]$transect <- 'Two'
+
+transects <- do.call(rbind.data.frame, transect.taxa)
+
+transects$class <- factor(transects$class, levels = c('PLSS', 'FIA'))
+
+trans.plot <- ggplot(transects, aes(x = cell, 
+                               y = value, 
+                               linetype = PFT, 
+                               color = variable)) +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_sqrt(expand = c(0,0)) +
+  #  stat_smooth(se = FALSE, method = 'gam', 
+  #              formula = y ~ te(x), family = binomial, size = 2) + 
+  stat_smooth(method = 'gam', family = betar, se = TRUE,
+              formula = y ~ s(x, k = 10), alpha = 0.1, size = 1.2) + 
+  stat_smooth(method = 'gam', family = betar, se = FALSE,
+              formula = y ~ s(x, k = 10), size = 1.2) + 
+  facet_wrap(~class+transect, nrow = 2) +
+  theme_bw() +
+  xlab('Meters East') + ylab('Percent Composition') +
+  theme(axis.text = element_text(family='serif', size = 16),
+        axis.title = element_text(family='serif', size = 18, face = 'bold'),
+        legend.text = element_text(family='serif', size = 14),
+        strip.text =  element_text(family='serif', size = 18, face = 'bold')) 
+
+colnames(transects)[3] <- 'taxon'
+transects$taxon <- as.character(transects$taxon)
+
+gam.test <- function(i){
+  tax <- taxon.spec$taxon[i]
+  tran <- taxon.spec$transect[i]
+  
+  input <- na.omit(subset(transects, transect == tran & taxon == tax))
+  
+  model1 <- gam(value ~ s(cell, by = class),
+                data = input,
+                family = betar)
+  model2 <- gam(value ~ s(cell),
+                data = input,
+                family = betar)
+  
+  diff(AIC(model1, model2)[,2])
+  
+}
+
+taxon.spec <- data.frame(transect = c('One', 'Two'),
+                         taxon    = rep(unique(transects$taxon), each = 2),
+                         AIC = NA)
+
+for(i in 1:nrow(taxon.spec)){
+  output <- gam.test(i)
+  taxon.spec$AIC[i] <- output[1]
+}
+
+#  Beta diversity along the gradient:
+comp.min <- comp.grid[plss.cells %in% fia.rows,]
+rownames(comp.min) <- plss.cells[plss.cells %in% fia.rows]
+
+fia.min <- fia.aligned[fia.rows%in% plss.cells,]
+rownames(fia.min) <- fia.rows[fia.rows %in% plss.cells]
+
+pls.beta <- betadiver(comp.min, 'sor')
+fia.beta <- betadiver(fia.min, 'sor')
+
+get_match <- function(cells, x, table){
+  matches   <- match(cells[[x]], rownames(table))
+  t(rbind(matches[-length(matches)], matches[-1]))
+}
+
+get_trans_t <- function(x){
+  pls.matches <- get_match(trans.cells, x, comp.min)
+  pls.trans.a <- as.matrix(pls.beta)[t(pls.matches)]
+  
+  fia.matches <- get_match(trans.cells, x, fia.min)
+  fia.trans.a <- as.matrix(fia.beta)[t(fia.matches)]
+  
+  t.test(fia.trans.a, pls.trans.a, paired=TRUE)
+}
+
+trans.t.tests <- lapply(1:2, get_trans_t)
