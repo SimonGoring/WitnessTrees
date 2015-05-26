68ee1765909d5430002eea00b7bca1271b34293e
Simon Goring <simon.j.goring@gmail.com>
2015-05-26 13:27:36 -0500

    Working on merging new version of the composition code.  This should get moved to a different repository so that it's all standardized.
    Changes reflect new version numbering in the code.
    `rasterizing_IN` and `rasterizing_il` have changes reflecting new column headers in the input files as well as reprojection of the base data.

--- R/composition_raster/rasterizing_IN.R
+++ R/composition_raster/rasterizing_IN.R
@@ -2,31 +2,32 @@
 library(rgdal)
 library(raster)
 
-indiana <- read.csv('data/raw_data/indiana/ndinpls_v1.3.csv')
+indiana <- read.csv('data/raw_data/indiana/ndinpls_v1.4.csv')
 
 #  We need to check to make sure the closest two trees are the first two.
 
 #  Clear leading and trailing spaces.
-indiana$L1.tree1 <- gsub('^[ \t]+|[ \t]+$', '', indiana$L1.tree1)
-indiana$L1.tree1 <- gsub('[ ]{2}', ' ', indiana$L1.tree1)
-indiana$L1.tree2 <- gsub('^[ \t]+|[ \t]+$', '', indiana$L1.tree2)
-indiana$L1.tree2 <- gsub('[ ]{2}', ' ', indiana$L1.tree2)
+indiana$L1_tree1 <- gsub('^[ \t]+|[ \t]+$', '', indiana$L1_tree1)
+indiana$L1_tree1 <- gsub('[ ]{2}', ' ', indiana$L1_tree1)
+indiana$L1_tree2 <- gsub('^[ \t]+|[ \t]+$', '', indiana$L1_tree2)
+indiana$L1_tree2 <- gsub('[ ]{2}', ' ', indiana$L1_tree2)
 
 #  Conversion table load and convert.
-taxon.conv <- read.csv('data/input/relation_tables/fullpaleon_conversion_v0.3-2.csv', header=TRUE, stringsAsFactors = FALSE)
+taxon.conv <- read.csv('data/input/relation_tables/fullpaleon_conversion_v0.3-3.csv', header=TRUE, stringsAsFactors = FALSE)
 
 taxon.conv[taxon.conv[,6] == '', 6] <- NA
 taxon.conv[taxon.conv[,6] %in% '#N/A', 6] <- 'Other hardwood'
 
-indiana$l3a.tree1 <-  taxon.conv[match(tolower(indiana$L1.tree1), 
+indiana$l3a.tree1 <-  taxon.conv[match(tolower(indiana$L1_tree1), 
                                        tolower(taxon.conv$Level.1)),6]
 
-indiana$l3a.tree2 <-  taxon.conv[match(tolower(indiana$L1.tree2), 
+indiana$l3a.tree2 <-  taxon.conv[match(tolower(indiana$L1_tree2), 
                                        tolower(taxon.conv$Level.1)),6]
 
-coordinates(indiana) <- ~ POINT_X + POINT_Y
+coordinates(indiana) <- ~ x + y
 
-proj4string(indiana) <- CRS('+init=epsg:4326')
+#proj4string(indiana) <- CRS('+init=epsg:4326')
+proj4string(indiana) <- CRS('+init=epsg:3175')
 
 base.rast <- raster(xmn = -71000, xmx = 2297000, ncols=296,
                     ymn = 58000,  ymx = 1498000, nrows = 180,
@@ -48,4 +49,4 @@ colnames(rast.set) <- taxa
 rast.coord <- xyFromCell(base.rast, 1:ncell(base.rast))
 rast.out <- cbind(rast.coord, rast.set)
 
-write.csv(rast.out, 'data/output/gridded/IndianaData_v0.2.csv')
+write.csv(rast.out, 'data/output/gridded/IndianaData_v0.3.csv')
