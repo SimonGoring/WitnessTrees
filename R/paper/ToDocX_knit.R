9467670b420cc89f75afaf49eaeec809035b7f4c
Simon <simon.j.goring@gmail.com>
2016-01-29 11:39:44 -0800

    Text edits throughout `GoringetalMarkdown.Rmd`, reflecting edits in response to reviewers & co-author edits.
    File cleanup in the `paper` directory.
    
    Added references to `goringetal_references`.
    
    Moved PLoS submission files into a new directory.

--- R/paper/ToDocX_knit.R
+++
@@ -1,7 +0,0 @@
-
-library(devtools)
-install_github('rmarkdown', 'rstudio')
-
-library(rmarkdown)
-render('GoringetalMarkdown.Rmd', 'word_document')
-render('GoringetalMarkdown.Rmd', 'pdf_document')
